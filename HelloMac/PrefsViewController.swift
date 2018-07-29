9b17642f890582fb5d7335d43bc5a06f1d1d3398
dominic <hello@dom.voyage>
2018-07-29 11:11:03 -0500

    added shortcuts

---
+++ HelloMac/PrefsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PrefsViewController.swift
+//  HelloMac
+//
+//  Created by Dominic Holmes on 7/29/18.
+//  Copyright © 2018 Dominic Holmes. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
